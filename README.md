736d0ab749e77357c9226c44102cf02b0ec6de81
sheetalrprasad <sheetalraniprasad@gmail.com>
2023-05-15 09:36:04 -0700

    M15

---
+++ README.md
@@ -0,0 +1,5 @@
+# CS 648 M15 Lab
+
+1. React
+2. Babel
+3. Webpackcls
--- app.js
+++ app.js
@@ -10,15 +10,13 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended:false }))
 app.use(bodyParser.json())
 app.use('/', routes)
-//app.get('/add', (req, res)=> res.render('add', {title: 'Add Employee'}))
-//app.use((req, res)=> res.status(404).render('404', {title: 'Page Not Found'}))
 
 const PORT = process.env.PORT || 5000
 
 const init = async() => {
     try {
         await connectDB(process.env.DB)
-        console.log("connected to the database...")
+        console.log("Connected to database...")
         app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
     } catch (error) {
        console.log(error) 
